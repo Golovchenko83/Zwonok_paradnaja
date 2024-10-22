03bcc6e68db5d986972a156bc5a606d4a909bad9
Golovchenko83 <km.slawa83@gmail.com>
2024-10-22 23:33:05 +0600

    5

--- src/zwonok_paradnaja.cpp
+++ src/zwonok_paradnaja.cpp
@@ -4,20 +4,32 @@
 #include <ArduinoOTA.h> // Библиотека для OTA-прошивки
 #include <PubSubClient.h>
 #include <TimerMs.h>
+#include <Wire.h>
+#include <ErriezDHT22.h>
 WiFiClient espClient;
 PubSubClient client(espClient);
+DHT22 dht22 = DHT22(D7);
 TimerMs OTA_Wifi(10, 1, 0);
-const char *name_client = "zwonok-paradnaja"; // Имя клиента и сетевого порта для ОТА
+TimerMs Dht_t(15000, 1, 0);
+const char *name_client = "zwonok-paradnaja";      // Имя клиента и сетевого порта для ОТА
 const char *mqtt_reset = "zwonok_paradnaja_reset"; // Имя топика для перезагрузки
-const char *ssid = "Beeline";              // Имя точки доступа WIFI
-const char *password = "sl908908908908sl"; // пароль точки доступа WIFI
+const char *ssid = "Beeline";                      // Имя точки доступа WIFI
+const char *password = "sl908908908908sl";         // пароль точки доступа WIFI
 const char *mqtt_server = "192.168.1.221";
 String s;
 long timeM;
 byte buff_clear;
 static const uint8_t Pin_zum = 12;
 static const uint8_t Pin_zum_d = 14;
-int data = 0;
+int data = 0, dht_tik = 0;
+float dht_raw = 0, dht_gr = 0, dht_sr = 0;
+
+void publish_send(const char *top, float &ex_data) // Отправка Показаний с сенсоров
+{
+  char send_mqtt[10];
+  dtostrf(ex_data, -2, 1, send_mqtt);
+  client.publish(top, send_mqtt, 1);
+}
 
 void signal1()
 {
@@ -118,6 +130,26 @@ void loop()
 {
   ESP.wdtFeed();
 
+  if (Dht_t.tick())
+  {
+    float temp_am_raw = 0;
+    if (dht22.available())
+    {
+      dht_tik++;
+      temp_am_raw = dht22.readTemperature();
+      temp_am_raw = (temp_am_raw / 10)-0.2;
+      dht_sr = dht_sr + temp_am_raw;
+      publish_send("Zwonok_temperature", temp_am_raw);
+    }
+    if (dht_tik == 60)
+    {
+      dht_sr = dht_sr / dht_tik;
+      publish_send("Zwonok_temperature_gr", dht_sr);
+      dht_tik = 0;
+      dht_sr = 0;
+    }
+  }
+
   if (OTA_Wifi.tick()) // Поддержание "WiFi" и "OTA"  и Пинок :) "watchdog" и подписка на "Топики Mqtt"
   {
     ArduinoOTA.handle();     // Всегда готовы к прошивке
@@ -129,9 +161,9 @@ void loop()
         ESP.wdtFeed();                   // Пинок :) "watchdog"
         if (client.connect(name_client)) // имя на сервере mqtt
         {
-          client.subscribe(mqtt_reset);  // подписались на топик 
+          client.subscribe(mqtt_reset);         // подписались на топик
           client.subscribe("zwonok_paradnaja"); // подписались на топик
-          client.subscribe(name_client); // подписались на топик
+          client.subscribe(name_client);        // подписались на топик
 
           // Отправка IP в mqtt
           char IP_ch[20];
@@ -153,5 +185,6 @@ void setup()
   wi_fi_con();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  ESP.wdtDisable();           // Активация watchdog
+  ESP.wdtDisable(); // Активация watchdog
+  dht22.begin();
 }
